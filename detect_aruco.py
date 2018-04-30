b9bff5d89f429403abb047ed88a6aa010ea28755
Patric Jensfelt <patric@kth.se>
2018-04-30 21:18:48 +0200

    Removed some code and prionting pose of only marker 1001

--- detect_aruco.py
+++ detect_aruco.py
@@ -10,14 +10,8 @@ dictionary = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_ARUCO_ORIGINAL)
 # Camera matrix and dsistortion parameters
 A = np.matrix([[538.9863478438378, 0, 328.326117302372], [0, 539.0749475096734, 209.4122266512357], [0, 0, 1]])
 dist_coeff = np.r_[0.06843722986120428, -0.1229275646593768, -0.01015740867557202, 0.00886817786361772, 0]
+marker_length = 0.0265
 
-print(A)
-print(dist_coeff)
-
-ids = (1001)
-obj_pts = (1)
-
-board = cv2.aruco.GridBoard_create(markersX=1, markersY=1, markerLength=0.02, markerSeparation=0.01, dictionary=dictionary, firstMarker=1001)
 while(True):
     # Capture frame-by-frame
     ret, frame = cap.read()
@@ -25,17 +19,25 @@ while(True):
     # Our operations on the frame come here
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     
-    res = cv2.aruco.detectMarkers(image=gray,dictionary=dictionary, cameraMatrix=A, distCoeff=dist_coeff)
-    print(res[0],res[1],len(res[2]))
+    corners, ids, rejected = cv2.aruco.detectMarkers(image=gray,dictionary=dictionary, cameraMatrix=A, distCoeff=dist_coeff)
+
+    #print(corners,ids,len(rejected))
     
-    if len(res[0]) > 0:
-        cv2.aruco.drawDetectedMarkers(gray,res[0],res[1])
-        ret = cv2.aruco.estimatePoseSingleMarkers(res[0], 0.0265, A, dist_coeff)
-        print(ret[0][0],ret[1][0])
-    
+    marker_1001_found = False
+    if len(corners) > 0:
+        cv2.aruco.drawDetectedMarkers(gray, corners, ids)
+        rvec, tvec, obj_pts = cv2.aruco.estimatePoseSingleMarkers(corners, marker_length, A, dist_coeff)
+        for i in [0,len(ids)-1]:
+            print(i)
+            if ids[i] == 1001:
+                marker_1001_found = True
+                print('Marker 1001 is at position:', rvec[i], tvec[i])
+    if not marker_1001_found:
+        print('Marker 1001 not found')
+
     # Display the resulting frame
     cv2.imshow('frame',gray)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    if cv2.waitKey(1) == ord('q')
         break
 
 # When everything done, release the capture
